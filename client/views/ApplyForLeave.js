e8eae06a1dba3da545f524063a74781ab6d9d331
cctech-labs <labs@cctech.co.in>
2017-09-19 17:42:44 +0530

    Apply leave form

--- client/views/ApplyForLeave.js
+++ client/views/ApplyForLeave.js
@@ -1,10 +1,11 @@
-Template.applyForLeave.onCreated(function () {
-
-});
-
-Template.applyForLeave.rendered = function () {            
-             
-
+  $(document).ready(function() {                     
+               
+    $('#datetimepicker5').datetimepicker({
+       format: 'DD/MM/YYYY'
+    });
+     $('#datetimepicker6').datetimepicker({
+       format: 'DD/MM/YYYY'
+    });  
     $('#contact_form').bootstrapValidator({
         // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
         feedbackIcons: {
@@ -153,66 +154,5 @@ Template.applyForLeave.rendered = function () {
                 console.log(result);
             }, 'json');
         });
-
-	$('#datetimepicker5').datepicker({
-       
-    });
-     $('#datetimepicker6').datepicker({
-       
-	});
-	
-
-};
-
-Template.applyForLeave.destroyed = function () {
-};
-
-Template.applyForLeave.helpers({
 });
 
-Template.applyForLeave.events({
-
-
-	
-	'click #applyForLeaveBtn'(event, instance){
-		var leaveType = $("#leaveTypeOption").val();
-		var leaveReason = $("#leaveReason").val();
-		var startDate = $("#minDate").val();
-		var endDate = $("#maxDate").val();
-		var noOfDays = $("#noOfDays").val();
-		if(!( Meteor.user() && Meteor.user().services.google.email.split('@')[1].includes('cctech.co.in') == true ) )
-		{
-			return;
-		}
-		else
-		{
-			emailId = Meteor.user().services.google.email;
-			empName = Meteor.user().services.google.name;
-		}
-
-		var Data = {
-			"Emp_Name": empName,
-			"Emp_Email": emailId,
-			"Leave_Type": leaveType,
-			"Reason": leaveReason,
-			"Start_Date": startDate,
-			"End_Date": endDate,
-			"No_Of_Days": parseInt(noOfDays),
-		}
-
-		$.ajax({
-			type: "POST",
-			url: "http://localhost:3000/leaves",
-			contentType: "application/json",
-			async: false,
-			data: JSON.stringify(Data),
-			success: function (result) {
-				//alert(result); 
-				console.log(result);
-			}
-
-		});
-
-
-	}
-});
